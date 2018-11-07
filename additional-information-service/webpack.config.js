a7b7bd812006ff16312430a190daa85809dc40f8
Brian Louie <brianalouie@gmail.com>
2018-11-07 13:37:51 -0800

    fixed?

---
+++ additional-information-service/webpack.config.js
@@ -0,0 +1,26 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public',
+  }
+};
