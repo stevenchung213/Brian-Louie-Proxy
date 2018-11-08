9177ee89da3b2b9c746e1d6abc1c93b9ece12991
Brian Louie <brianalouie@gmail.com>
2018-11-07 17:45:48 -0800

    fixed port again

--- additional-information-service/src/index.js
+++ additional-information-service/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import { PreQuery } from './Components/PreQuery.js';
+import normalizePort from 'normalize-port';
+
+const port = normalizePort(process.env.PORT || '8081');
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8081/graphql',
+  uri: `http://localhost:${port}/graphql`,
 });
 
 ReactDOM.render(
