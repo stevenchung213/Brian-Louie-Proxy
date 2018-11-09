e69d2232f9b897a89edcefbfa2ba4d6e1cd4929f
Brian Louie <brianalouie@gmail.com>
2018-11-08 19:32:59 -0800

    maybe all working

--- Product-Image-Gallery-Service/database/index.js
+++ Product-Image-Gallery-Service/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/gallery';
+const mongoUri = 'mongodb+srv://admin1:admin1password@cluster0-ytvdt.mongodb.net/gallery?retryWrites=true';
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 
