7c7c086c72c4a9906d15ae1410df7045fdd90b34
Steven Chung <stevenchung.213@gmail.com>
2018-11-15 18:36:37 -0800

    database seed generation

--- additional-information-service/server/database/data-seed.js
+++ additional-information-service/server/database/data-seed.js
@@ -1,77 +1,107 @@
 const faker = require('faker');
-const mongoose = require('mongoose');
-const House = require('./House.js');
+// const mongoose = require('mongoose');
+// const House = require('./House.js');
 const db = require('./index.js');
+const csv = require('fast-csv');
+const fs = require('fs');
 
 const random = num => Math.ceil(Math.random() * num);
 
-const zestHistory = () => {
-  let total = 300000;
-  const years = 8 + random(2);
-  const months = random(12);
-  let count = 0;
-  const spike = [12, 7, 12, 5, 8, 5, 14, 3, 19, 1000];
-  const slope = [
-    -4000,
-    -3000,
-    -1000,
-    2000,
-    5000,
-    2000,
-    5000,
-    3000,
-    10000,
-    7000,
-    700,
-    -700,
-  ];
-  let moreSlope = 0;
+// const zestHistory = () => {
+//   let total = 300000;
+//   const years = 8 + random(2);
+//   const months = random(12);
+//   let count = 0;
+//   const spike = [12, 7, 12, 5, 8, 5, 14, 3, 19, 1000];
+//   const slope = [
+//     -4000,
+//     -3000,
+//     -1000,
+//     2000,
+//     5000,
+//     2000,
+//     5000,
+//     3000,
+//     10000,
+//     7000,
+//     700,
+//     -700,
+//   ];
+//   let moreSlope = 0;
+//
+//   return Array.from({ length: years * 12 + months }, () => {
+//     count++;
+//     if (count % spike[0] === 0) {
+//       const rand = random(4);
+//       moreSlope = rand > 2 ? 2000 : rand === 2 ? -2000 : 0;
+//       if (spike[0] === 14) {
+//         moreSlope = 8000;
+//       }
+//       spike.shift();
+//     }
+//     total += slope[Math.floor(count / 12)] + moreSlope;
+//
+//     return total + random(7000);
+//   });
+// };
 
-  return Array.from({ length: years * 12 + months }, () => {
-    count++;
-    if (count % spike[0] === 0) {
-      const rand = random(4);
-      moreSlope = rand > 2 ? 2000 : rand === 2 ? -2000 : 0;
-      if (spike[0] === 14) {
-        moreSlope = 8000;
-      }
-      spike.shift();
-    }
-    total += slope[Math.floor(count / 12)] + moreSlope;
 
-    return total + random(7000);
-  });
-};
+
+const statusArray = ['For Sale', 'Sold'];
 
 const seedFunc = () => {
   let count = 0;
-  return Array.from({ length: 100 }, () => {
+  return Array.from({length: 1500000}, () => {
     const id = count;
     count++;
 
-    const zestimate = zestHistory();
+    // const zestimate = zestHistory();
     return {
-      _id: id.toString(),
-      address: faker.address.streetAddress(),
-      city: faker.address.city(),
+      propertyid: id,
+      address: `'${faker.address.streetAddress()}'`,
+      city: `'${faker.address.city()}'`,
       zip: 98100 + random(99),
-      zestimate,
+      // zestimate,
       beds: 3 + Math.floor(Math.random() * 2.5),
       baths: 2.5 + 0.5 * Math.floor(Math.random() * 3),
-      sqFt: 1150 + 10 * random(20),
-      status: Math.random() < 0.5 ? 'For Sale' : 'Sold',
-      taxAssessment: zestimate[zestimate.length - 1] * 0.937,
+      sqft: 1150 + 10 * random(20),
+      status: `'${statusArray[Math.floor(Math.random() * 2)]}'`
+      // , taxAssessment: zestimate[zestimate.length - 1] * 0.937,
     };
   });
 };
 
 const seed = seedFunc();
-module.exports = seedFunc;
 
-const seedDatabase = () => {
-  House.create(seed)
-    .then(() => mongoose.connection.close())
-    .catch(err => console.error(err));
+
+const sdcSeed = () => {
+  const csvStream = csv.createWriteStream({headers: false}),
+    writableStream = fs.createWriteStream("my7.csv");
+
+  writableStream.on("finish", () => {
+    console.log('OMG FINALLY DONE!');
+  });
+
+  csvStream.pipe(writableStream);
+
+  for (let i = 0; i < 1500000; i++) {
+    csvStream.write(seed[i]);
+  }
+
+  csvStream.end();
 };
 
-seedDatabase();
+
+sdcSeed();
+
+
+
+module.exports = seedFunc;
+
+// const seedDatabase = () => {
+//   House.create(seed)
+//     .then(() => mongoose.connection.close())
+//     .catch(err => console.error(err));
+// };
+//
+// seedDatabase();
--- additional-information-service/server/database/index.js
+++ additional-information-service/server/database/index.js
@@ -1,10 +1,126 @@
-const mongoose = require('mongoose');
+const cassandra = require('express-cassandra');
+// cass = models
+const cass = cassandra.createClient({
+  clientOptions: {
+    contactPoints: ['127.0.0.1'],
+    protocolOptions: { port: 9042 },
+    keyspace: 'sdc',
+    queryOptions: { consistency: cassandra.consistencies.one },
+  },
+  ormOptions: {
+    defaultReplicationStrategy: {
+      class: 'SimpleStrategy',
+      replication_factor: 1,
+    },
+    migration: 'safe',
+  },
+});
 
-const uri = 'mongodb+srv://admin1:admin1password@cluster0-ytvdt.mongodb.net/houses?retryWrites=true/';
 
-const db = mongoose.connect(
-  uri,
-  { useNewUrlParser: true },
-);
+const schema = cass.loadSchema('zillwoah', {
+  fields: {
+    propertyid: 'int',
+    address: 'text',
+    zip: 'int',
+    beds: 'decimal',
+    baths: 'decimal',
+    sqft: 'decimal',
+    status: 'text'
+  },
+  key: ['propertyid'],
+});
 
-module.exports = db;
+
+schema.syncDB((err, result) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('schema was changed', result);
+});
+
+
+
+const csvSeed = `COPY zillwoah FROM 'C:\\my1.csv' WITH DELIMITER=',' AND HEADER=FALSE`;
+
+
+cass.instance.schema.execute_query(csvSeed);
+
+
+
+
+
+
+
+// POSTGRES
+//
+// const { Pool } = require('pg');  // non local host
+// const pg = require('pg');
+// // connection string
+// const connection = "postgres://postgres:102884@localhost:5432/sdc";
+// // new connection
+// const db = new pg.Client(connection);
+//
+// // connect
+// db.connect();
+//
+//
+// const sdc =
+//   `CREATE TABLE IF NOT EXISTS zillwoah (
+//
+//     propertyid int,
+//     address varchar(84),
+//     city varchar(28),
+//     zip integer,
+//     beds decimal,
+//     baths decimal,
+//     sqft decimal,
+//     status varchar(10)
+//
+// )`;
+//
+// db.query(sdc)
+//   .then(res => {
+//     console.log(res);
+//     db.end();
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     db.end()
+//   });
+//
+//
+// const csvSeed = `COPY zillwoah FROM 'C:\\my.csv' WITH (FORMAT CSV);`;
+//
+//
+// db.query(csvSeed)
+//   .then(res => {
+//     console.log(res);
+//     db.end();
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     db.end();
+//   });
+//
+//
+// module.exports = db;
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+//
+// const uri = 'mongodb+srv://admin1:admin1password@cluster0-ytvdt.mongodb.net/houses?retryWrites=true/';
+//
+// const db = mongoose.connect(
+//   uri,
+//   { useNewUrlParser: true },
+// );
+//
+// module.exports = db;
