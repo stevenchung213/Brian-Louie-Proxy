86f9511a271c0451841a59e1cc3da33f98cfa098
Steven Chung <stevenchung.213@gmail.com>
2018-11-23 21:00:18 -0800

    cassandra seed via csv update

--- additional-information-service/server/database/data-seed.js
+++ additional-information-service/server/database/data-seed.js
@@ -45,13 +45,13 @@ const random = num => Math.ceil(Math.random() * num);
 //   });
 // };
 
-
+console.time();
 
 const statusArray = ['For Sale', 'Sold'];
 
 const seedFunc = () => {
-  let count = 0;
-  return Array.from({length: 1500000}, () => {
+  let count = 9000001;
+  return Array.from({length: 1000000}, () => {
     const id = count;
     count++;
 
@@ -59,32 +59,33 @@ const seedFunc = () => {
     return {
       propertyid: id,
       address: `'${faker.address.streetAddress()}'`,
-      city: `'${faker.address.city()}'`,
-      zip: 98100 + random(99),
-      // zestimate,
-      beds: 3 + Math.floor(Math.random() * 2.5),
       baths: 2.5 + 0.5 * Math.floor(Math.random() * 3),
+      beds: 3 + Math.floor(Math.random() * 2.5),
+      city: `'${faker.address.city()}'`,
       sqft: 1150 + 10 * random(20),
-      status: `'${statusArray[Math.floor(Math.random() * 2)]}'`
+      status: `'${statusArray[Math.floor(Math.random() * 2)]}'`,
+      zip: 98100 + random(99)
+      // zestimate,
       // , taxAssessment: zestimate[zestimate.length - 1] * 0.937,
     };
   });
 };
 
 const seed = seedFunc();
+console.timeEnd();
 
-
+console.time();
 const sdcSeed = () => {
   const csvStream = csv.createWriteStream({headers: false}),
-    writableStream = fs.createWriteStream("my7.csv");
+    writableStream = fs.createWriteStream("my10.csv");
 
   writableStream.on("finish", () => {
-    console.log('OMG FINALLY DONE!');
+    console.log('CSV DONE!');
   });
 
   csvStream.pipe(writableStream);
 
-  for (let i = 0; i < 1500000; i++) {
+  for (let i = 0; i < 1000000; i++) {
     csvStream.write(seed[i]);
   }
 
@@ -94,9 +95,9 @@ const sdcSeed = () => {
 
 sdcSeed();
 
+console.timeEnd();
 
-
-module.exports = seed;
+// module.exports = seed;
 
 // const seedDatabase = () => {
 //   House.create(seed)
--- additional-information-service/server/database/index.js
+++ additional-information-service/server/database/index.js
@@ -1,59 +1,127 @@
-const seed = require('data-seed.js');
-const cassandra = require('express-cassandra');
-// cass = models
-const models = cassandra.createClient({
-  clientOptions: {
-    contactPoints: ['127.0.0.1'],
-    protocolOptions: { port: 9042 },
-    keyspace: 'sdc',
-    queryOptions: { consistency: cassandra.consistencies.one },
-  },
-  ormOptions: {
-    defaultReplicationStrategy: {
-      class: 'SimpleStrategy',
-      replication_factor: 1,
-    },
-    migration: 'safe',
-  },
+const cassandra = require('cassandra-driver');
+const async = require('async');
+const seed = require('./data-seed.js');
+const distance = cassandra.types.distance;
+// const loadBalancing = new cassandra.policies.loadBalancing.DCAwareRoundRobinPolicy('local');
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'],
+  localDataCenter: 'datacenter1',
+  // pooling: {
+  //   coreConnectionsPerHost: {
+  //     [distance.local]: 1
+  //   }
+  // },
+  // policies: {
+  //   loadBalancing: loadBalancing
+  // }
 });
 
+console.time();
 
-const Model = models.loadSchema('zillwoah', {
-  fields: {
-    propertyid: 'int',
-    address: 'text',
-    zip: 'int',
-    beds: 'decimal',
-    baths: 'decimal',
-    sqft: 'decimal',
-    status: 'text'
-  },
-  key: ['propertyid'],
-});
+client.connect()
+  .then(() => {
+    const createKS = "CREATE KEYSPACE IF NOT EXISTS sdc WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : '1' }";
+    return client.execute(createKS);
+  })
+  .then(() => {
+    const createTable = "CREATE TABLE IF NOT EXISTS sdc.zillwoah ( propertyid int PRIMARY KEY, address text, city text, zip int, beds" +
+      " decimal," +
+      " baths decimal," +
+      " sqft decimal, status text )";
+    return client.execute(createTable);
+  })
+  // .then(() => {
+  //   const insert = 'INSERT INTO sdc.zillwoah (propertyid, address, city, zip, beds, baths, sqft, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+  //   for (let i = 0; i < 10000; i++) {
+  //     client.execute(insert, Object.values(seed[i]), {prepare: true});
+  //   }
+  // })
+  .then(() => {
+    const test = 'SELECT * from sdc.zillwoah WHERE propertyid = ?';
+    return client.execute(test, [10000], {prepare: true});
+  })
+  .then(result => {
+    const row = result.first();
+    console.log('Retrieved row: %j', row);
+    return client.shutdown();
+  })
+  .catch(err => {
+    console.error('There was an error', err);
+    return client.shutdown().then(() => { throw err; });
+  });
+
+console.timeEnd();
 
 
-Model.syncDB((err, result) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log('schema was changed', result);
-});
+// client.connect((err, res) => {
+//   if (err) { console.log(err) }
+//   console.log('connected to cassandra db ' + res);
+// });
+//
+// client.execute(createType)
+//   .then(() => {
+//     console.log('ks created');
+//   })
+//   .catch(err => { console.log(err) });
+
+// .then(() => client.execute(createKS))
+// .then(() => client.execute("USE sdc"))
+// .then(() => client.execute(createType))
+// .catch(err => console.log(err));
+//
 
 
-const cassSeed = (seed) => {
-  let result = [];
-  for (let i = 0; i < 10000000; i++) {
+// const seed = require('./data-seed.js');
+// const cassandra = require('express-cassandra');
+// // cass = models
+// const models = cassandra.createClient({
+//   clientOptions: {
+//     contactPoints: ['127.0.0.1'],
+//     protocolOptions: { port: 9042 },
+//     keyspace: 'sdc',
+//     queryOptions: { consistency: cassandra.consistencies.one },
+//   },
+//   ormOptions: {
+//     defaultReplicationStrategy: {
+//       class: 'SimpleStrategy',
+//       replication_factor: 1,
+//     },
+//     migration: 'safe',
+//   },
+// });
+//
+//
+// const Model = models.loadSchema('zillwoah', {
+//   fields: {
+//     propertyid: 'int',
+//     address: 'text',
+//     zip: 'int',
+//     beds: 'decimal',
+//     baths: 'decimal',
+//     sqft: 'decimal',
+//     status: 'text'
+//   },
+//   key: ['propertyid'],
+// });
+//
+//
+// Model.syncDB((err, result) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log('schema was changed', result);
+// });
 
-  }
-};
 
-
+// const cassSeed = (seedArray) => {
+//
+//   for (let i = 0; i < 10000000; i++) {
+//
+//   }
+// };
 
 
 // models.instance.schema.execute_query();
-
-
-
 
 
 // // POSTGRES
@@ -117,7 +185,6 @@ const indexing = `CREATE INDEX propertyid ON zillwoah;`;
 const find = `SELECT * `
 
 // module.exports = db;
-
 
 
 // const mongoose = require('mongoose');
