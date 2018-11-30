aea4c9ee9c4996f3e8b0b7531a7018df08bd528c
Steven Chung <stevenchung.213@gmail.com>
2018-11-29 18:52:38 -0800

    final decision (5) 2m record csv files

---
+++ additional-information-service/server/database/2.js
@@ -0,0 +1,55 @@
+const csv = require('fast-csv');
+const fs = require('fs');
+
+const propertytax = [1.2, 1.5, 1.7, 2, 2.3, 2.7];
+
+
+console.time('generation');
+
+const seedFunc = () => {
+  let count = 2000001;
+  return Array.from({length: 2000000}, () => {
+    const id = count;
+    const price = Math.round((Math.random() * 500000) + 500000);
+    const proptax =  propertytax[Math.floor(Math.random() * propertytax.length)];
+    count++;
+
+    return {
+      propertyid: id,
+      downpayment: Math.round((Math.random() * 100000) + 100000),
+      hoa: Math.round((Math.random() * 1200) + 1200),
+      price: price,
+      propertytaxpercent: proptax,
+      propertytaxx: Math.round(price * (proptax * (0.01)))
+
+    };
+  });
+};
+
+
+const seed = seedFunc();
+
+console.timeEnd('generation');
+
+
+const sdcSeed = () => {
+  console.time('writecsv');
+  const csvStream = csv.createWriteStream({headers: false}),
+    writableStream = fs.createWriteStream("seed2.csv");
+
+  writableStream.on("finish", () => {
+    console.log('CSV DONE!');
+  });
+
+  csvStream.pipe(writableStream);
+
+  for (let i = 0; i < 2000000; i++) {
+    csvStream.write(seed[i]);
+  }
+
+  csvStream.end();
+  console.timeEnd('writecsv');
+};
+
+
+sdcSeed();
